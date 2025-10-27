1b3f71c51b0d693f914fc0c1aaf0c4c5a050770d
arunak007-sys <arun2017ak@gmail.com>
2025-10-27 17:54:32 +0530

    install apk

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(), tailwindcss(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Camera App',
@@ -19,12 +19,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/web-app-manifest-192x192',
+            src: '/web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/web-app-manifest-512x512',
+            src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
